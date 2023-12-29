8fdd0e8496643eeacbfad9c28596ce82806783e5
uemrey0 <ufukemreyuceturk@hotmail.com>
2023-12-29 17:26:28 +0300

    Add button counter and remove bottun with animation

--- components/ProductCart.js
+++ components/ProductCart.js
@@ -1,12 +1,44 @@
-// ProductCard.js
-import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Animated } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'; // İkon paketini yükleyin
 
 const ProductCard = ({ product }) => {
-  // Ürün kartına tıklandığında gerçekleşecek işlem
-  const onAddToCart = () => {
-    // Burada sepete ekleme işlemi yapabilirsiniz
+  const [count, setCount] = useState(0);
+  const heightAnim = useRef(new Animated.Value(0)).current; // Kutunun yükseklik animasyonu
+
+  useEffect(() => {
+    console.log(count)
+    if (count === 0) {
+        Animated.timing(heightAnim, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: false
+          }).start();
+        
+      } else {
+        Animated.timing(heightAnim, {
+            toValue: 1,
+            duration: 300,
+            useNativeDriver: false // Bu değer, kullanılan RN sürümüne göre değişebilir
+          }).start();
+      }
+  }, [count])
+
+  const toggleCounterPlus = () => {
+    setCount(c => c+1)
+  };
+
+  const toggleCounterMinus = () => {
+    setCount(c => c-1)
+  };
+
+
+
+  const heightStyle = {
+    height: heightAnim.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 80] // Animasyonlu kutunun maksimum yüksekliği
+    })
   };
 
   return (
@@ -16,14 +48,27 @@ const ProductCard = ({ product }) => {
       <View style={styles.infoContainer}>
         <Text style={styles.price}>{product.price}</Text>
         <Text style={styles.name}>{product.name}</Text>
-        <Text style={styles.count}>{product.count} adet</Text>
-        
       </View>
-      
     </View>
-    <TouchableOpacity onPress={onAddToCart} style={styles.addButton}>
-    <Icon name="add" size={20} color="#e53935" />
-  </TouchableOpacity>
+    <View style={styles.counterContainer}>
+    <TouchableOpacity onPress={toggleCounterPlus} style={styles.button}>
+      <Icon name="add" size={20} color="#e53935" />
+    </TouchableOpacity>
+    <Animated.View style={[styles.counter, heightStyle]}>
+      {count > 0 && (
+        <>
+          <View style={styles.button}>
+            <Text style={styles.count}>{count}</Text>
+          </View>
+          
+          <TouchableOpacity onPress={toggleCounterMinus} style={styles.button}>
+            <Icon name="remove" size={20} color="#e53935" />
+          </TouchableOpacity>
+          
+        </>
+      )}
+    </Animated.View>
+  </View>
   </>
   );
 };
@@ -35,9 +80,8 @@ const styles = StyleSheet.create({
     borderColor: "lightgrey",
     borderWidth: 1,
     padding: 10,
-    margin: 10, // Bu, kart aralarında boşluk sağlar.
-    overflow: 'hidden', // Bu, borderRadius etrafındaki gölgelerin ve kenarların düzgün görünmesini sağlar.
-    // Gölge stilleri
+    margin: 10,
+    overflow: 'hidden',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.22,
@@ -46,39 +90,59 @@ const styles = StyleSheet.create({
   },
   image: {
     width: '100%',
-    height: 70, // Görselin yüksekliği
+    height: 70,
     resizeMode: 'contain',
   },
   infoContainer: {
-    padding: 10, // İçerideki öğeler arasında boşluk sağlar.
+    padding: 10,
   },
   name: {
     fontSize: 14,
     color: '#333',
   },
-  count: {
-    fontSize: 12,
-    color: 'grey',
-    marginVertical: 4,
-  },
   price: {
     fontSize: 16,
-    color: '#FB8C00',
+    color: '#e53935',
     fontWeight: 'bold',
     marginVertical: 4,
   },
-  addButton: {
+  counterContainer: {
     position: 'absolute',
-    right: 0,
+    right: 2,
+    top: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  counter: {
+    backgroundColor: 'transparent',
+  },
+  changeButton: {
+    position: 'absolute',
+    left: 0,
     top: 0,
+    zIndex: 1,
+  },
+  count: {
+    fontSize: 16,
+    color: '#e53935',
+    alignSelf: 'center',
+  },
+  button: {
     backgroundColor: '#F8E8EE',
     borderRadius: 10,
-    width: 40,
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 5,
+    marginHorizontal: 10,
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-  // Diğer stiller...
 });
 
 export default ProductCard;
