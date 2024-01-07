e67a0c403896309e2033aa4047344d066532c75c
Ufuk Emre Yücetürk <ufukemreyuceturk@hotmail.com>
2024-01-07 22:37:51 +0300

    Loading screen ui update

--- screens/Loading.js
+++ screens/Loading.js
@@ -1,7 +1,8 @@
-// screens/LoadingScreen.js
-import React, { useEffect, useState } from 'react';
-import { View, ActivityIndicator, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, ActivityIndicator, Text, StyleSheet, Image, Dimensions } from 'react-native';
 import { checkFirstLaunch } from '../utils';
+
+const { width } = Dimensions.get('window');
 
 const Loading = ({navigation}) => {
   useEffect(() => {
@@ -13,12 +14,18 @@ const Loading = ({navigation}) => {
       }
     });
   }, []);
-    return (
-        <View style={styles.container}>
-            <ActivityIndicator size="large" />
-            <Text>Uygulama yükleniyor...</Text>
-        </View>
-    );
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{uri: "https://www.aeo.org.tr/Helpers/DuyuruIcon.ashx?yayinyeri=sayfaicerik&Id=36690"}} // Burada kendi logonuzu ekleyin
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <ActivityIndicator size="large" color="#e53935" /> {/* Renk ve boyut özelleştirilebilir */}
+      <Text style={styles.text}>Uygulama yükleniyor...</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -26,6 +33,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f0f0f0', // Arka plan rengini değiştirebilirsiniz
+  },
+  logo: {
+    width: width * 0.5, // Logonun ekran genişliğine oranla boyutu
+    height: width * 0.5, // Logonun ekran genişliğine oranla yüksekliği
+    marginBottom: 30,
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 18,
+    color: '#666', // Metin rengini değiştirebilirsiniz
   },
 });
 
